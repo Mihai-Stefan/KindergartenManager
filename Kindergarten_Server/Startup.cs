1dccdccc93b55351485f35aa53df41984a133283
Mihai-Stefan <mihaiste@gmail.com>
2021-08-23 21:25:55 +0300

    Identity added [from console]

--- Kindergarten_Server/Startup.cs
+++ Kindergarten_Server/Startup.cs
@@ -68,8 +68,12 @@ namespace Kindergarten_Server
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
