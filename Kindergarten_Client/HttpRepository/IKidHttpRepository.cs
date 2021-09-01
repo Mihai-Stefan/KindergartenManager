fe9b52f8bf18677f869111f21dab0beca86cd569
Mihai-Stefan <mihaiste@gmail.com>
2021-09-01 20:55:14 +0300

    NuGetPackages cleaned-up

---
+++ Kindergarten_Client/HttpRepository/IKidHttpRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Client.HttpRepository
+{
+    public interface IKidHttpRepository
+    {
+        Task<List<Kid>> GetKids();
+    }
+}
---
+++ Kindergarten_Client/HttpRepository/KidHttpRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Client.HttpRepository
+{
+    public class KidHttpRepository : IKidHttpRepository
+    {
+        private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _options;
+
+        public KidHttpRepository(HttpClient client)
+        {
+            _client = client;
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<List<Kid>> GetKids()
+        {
+            var response = await _client.GetAsync("kids");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            var kids = JsonSerializer.Deserialize<List<Kid>>(content, _options);
+            return kids;
+        }
+    }
+}
+
+
+
+
+
